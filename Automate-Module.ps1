72cefe823de776edd8aec34f71ed749209e22bda
belegdal <bigfoot.alighieri@gmail.com>
2020-06-16 16:59:27 -0400

    Update Automate-Module.ps1

--- Automate-Module.ps1
+++ Automate-Module.ps1
@@ -449,7 +449,7 @@ Function Install-Automate {
     If (([int]((Get-WmiObject Win32_OperatingSystem).BuildNumber) -gt 6000) -and ((get-host).Version.ToString() -ge 3)) {$AutomateURL = "https://$($Server)"} Else {$AutomateURL = "http://$($Server)"}
     $AutomateURLTest = "$($AutomateURL)/LabTech/"
     $SoftwarePath = "C:\Support\Automate"
-    $DownloadPath = "$($AutomateURL)/LabTech/Deployment.aspx?InstallerToken=0a7bec98803442e081035916109753d8"
+    $DownloadPath = "$($AutomateURL)/LabTech/Deployment.aspx?InstallerToken=83668ced4fe545538570478d79a79ee4"
     $Filename = "Automate_Agent.msi"
     $SoftwareFullPath = "$SoftwarePath\$Filename"
     Write-Verbose "Checking if Automate Server URL is active. Server entered: $($Server)"
